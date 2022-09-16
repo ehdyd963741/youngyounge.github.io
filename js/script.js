242d62343af39da3579cc0e4ba53cfb6299e7270
Dyoung <wkftkfwk0118@gmail.com>
2022-09-16 17:32:48 +0900

    0916포트폴리오작업중

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-window.onload = function(){
-  <script>
-  AOS.init();
-</script>
+window.onload = function(){  
+
+new Swiper('.abc', {
+  effect: "cube",
+  grabCursor: true,
+  loop:true,
+  speed: 1000,
+  autoplay: {
+    delay: 2000
+  },
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  }
+});
+
+
 }
