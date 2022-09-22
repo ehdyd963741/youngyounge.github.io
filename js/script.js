9bd1d293b2a23b216707e2cd4ff5844cec2b77e6
Dyoung <wkftkfwk0118@gmail.com>
2022-09-22 16:15:23 +0900

    0922포트폴리오작업중

--- js/script.js
+++ js/script.js
@@ -16,5 +16,47 @@ new Swiper('.abc', {
   }
 });
 
+const $text = document.querySelector(".auto-text");
+const $text2 = document.querySelector(".auto-text2");
+const $text3 = document.querySelector(".auto-text3");
+// 글자 모음
+const letters = ["Always", "From", "DevelopMent", "WooJunKim"];
+const letters2 = ["From"];
+const letters3 = ["DevelopMent"];
+// 글자 입력 속도
+const speed = 100;
+let i = 0;
+// 타이핑 효과
+const typing = async () => {  
+  const letter = letters[i].split("");
+  while (letter.length) {
+    await wait(speed);
+    $text.innerHTML += letter.shift(); 
+  }
+  // 잠시 대기
+  await wait(500);
+  // 지우는 효과
+  if (letters[i + 1]) remove();
+}
 
+// 글자 지우는 효과
+const remove = async () => {
+  const letter = letters[i].split("");
+  while (letter.length) {
+    await wait(speed);
+    letter.pop();
+    $text.innerHTML = letter.join(""); 
+  }
+  // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+  i++;
+  typing();
+}
+
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+  return new Promise(res => setTimeout(res, ms))
+}
+
+// 초기 실행
+setTimeout(typing, 2700);
 }
